856832a3ccfd83dc0dba57d1ae08474b2c09c2a0
Benjamin Gelis <benjamingelis@gmail.com>
2026-02-07 09:33:32 +0100

    feat: [US-011] - Add cache invalidation after activity creation
    - Add refetchQueries to useMutation in ActivityForm
    - Configure to refetch GetActivities, GetLatestActivities, GetActivitiesByCity
    - Add 4 unit tests verifying refetchQueries configuration
    - Add ResizeObserver mock to setupTests for Mantine component testing

--- front-end/src/setupTests.ts
+++ front-end/src/setupTests.ts
@@ -1,1 +1,23 @@
 import "@testing-library/jest-dom/extend-expect";
+
+// Mock window.matchMedia for Mantine components
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
+
+// Mock ResizeObserver for Mantine/Radix components
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
